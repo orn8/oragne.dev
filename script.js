b5b01a5633c5e417c6aa22444b06884012550072
oragne <me@oragne.dev>
2024-11-23 13:40:04 +1100

    add space

--- script.js
+++ script.js
@@ -13,18 +13,21 @@ document.addEventListener("DOMContentLoaded", () => {
     nameContainer.target = "_blank";
     nameContainer.rel = "noopener noreferrer";
 
+    const repoIcon = document.createElement("img");
+    repoIcon.src = "https://raw.githubusercontent.com/orn8/oragne.dev/refs/heads/main/assets/repo-icon.png";
+    repoIcon.alt = "GitHub Repo Icon";
+    repoIcon.classList.add("repo-icon");
+
+    const space = document.createTextNode(" ");
+    repo.prepend(space);
+    repo.prepend(repoIcon);
+
     repo.appendChild(nameContainer);
 
     const descElement = document.createElement("p");
     descElement.classList.add("repo-desc");
     descElement.textContent = description;
     repo.appendChild(descElement);
-
-    const repoIcon = document.createElement("img");
-    repoIcon.src = "https://raw.githubusercontent.com/orn8/oragne.dev/refs/heads/main/assets/repo-icon.png";
-    repoIcon.alt = "GitHub Repo Icon";
-    repoIcon.classList.add("repo-icon");
-    repo.prepend(repoIcon);
 
     repo.style.transition = `opacity 1s ease-in-out, transform 1s ease-in-out`;
     setTimeout(() => {

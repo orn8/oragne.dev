c0758f0715ebeb21880c1c34364469713683af90
oragne <me@oragne.dev>
2024-11-24 10:39:31 +1100

    app menu..?

--- script.js
+++ script.js
@@ -49,3 +49,24 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+// App Menu
+document.addEventListener("DOMContentLoaded", () => {
+  const menuButton = document.getElementById("app-menu-button");
+  const menuGrid = document.getElementById("app-menu-grid");
+
+  menuButton.addEventListener("click", () => {
+    menuGrid.classList.toggle("show");
+  });
+
+  const appItems = document.querySelectorAll(".app-menu-item");
+
+  appItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      const link = item.dataset.link;
+      if (link) {
+        window.open(link, "_blank");
+      }
+    });
+  });
+});
